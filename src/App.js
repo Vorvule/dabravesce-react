8b9139c59811f7a30fd2ba1a73d3beeda1818072
Vorvule <7105690@gmail.com>
2021-11-21 01:05:30 +0300

    W3Schools' Social Network Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Top from "./components/Top";
+import TopMobile from "./components/TopMobile";
+import PageContainer from "./components/PageContainer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w3-theme-15">
+      <Top />
+      <TopMobile />
+      <PageContainer />
+      <Footer />
+      {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
+      */}
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <br />
+      <footer className="w3-container w3-theme-d3 w3-padding-16">
+        <h5>Footer</h5>
+      </footer>
+
+      <footer className="w3-container w3-theme-d5">
+        <p>
+          Powered by{" "}
+          <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">
+            w3.css
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
