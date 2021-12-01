ac49dd7b5a9eb77a58e0d12d1eb7911fba2b99f9
Vorvule <7105690@gmail.com>
2021-12-01 12:51:09 +0300

    toggleCollapse function introduced

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import MenuVolumeList from "./MenuVolumeList";
 import allVolumes from "../../tomes/all_volumes";
 
+const toggleShowVolume = (idEn) => {
+  console.log("toggle", idEn);
+};
+
 const Menu = () => (
   <ul className="w3-ul">
-    <MenuVolumeList volumeList={allVolumes} />
+    <MenuVolumeList volumeList={allVolumes} onToggle={toggleShowVolume} />
   </ul>
 );
 
--- src/components/menu/MenuVolumeList.jsx
+++ src/components/menu/MenuVolumeList.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import MenuBookList from "./MenuBookList";
 
-const MenuVolumeList = ({ volumeList }) => {
-  const [menuBookListHidden, setMenuBookListHidden] = useState(true);
-  const toggleShowVolumeContent = () => {
-    setMenuBookListHidden(!menuBookListHidden);
+const MenuVolumeList = ({ volumeList, onToggle }) => {
+  const initialCollapseState = {
+    Gospels: false,
+    Apostles: false,
+    Epistles: false,
+  };
+  const [collapsedState, setCollapsedState] = useState(initialCollapseState);
+
+  const toggleCollapse = (idEn) => {
+    collapsedState[idEn] = !collapsedState[idEn];
+    setCollapsedState(() => collapsedState);
   };
 
   return volumeList.map((volume) => {
     return (
       <div key={volume.idEn}>
-        <li onClick={toggleShowVolumeContent}>{volume.name}</li>
-        <ul className={`w3-ul ${menuBookListHidden && "w3-hide"}`}>
+        <li onClick={() => toggleCollapse(volume.idEn)}>{volume.name}</li>
+        <ul className={`w3-ul ${collapsedState[volume.idEn] && "w3-hide"}`}>
           <MenuBookList bookList={volume.text} />
         </ul>
       </div>
