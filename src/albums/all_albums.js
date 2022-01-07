c6963100c18349cd926b8c55f7757766ea8cde82
Vorvule <7105690@gmail.com>
2022-01-07 20:41:46 +0300

    Prayer book added

--- src/albums/all_albums.js
+++ src/albums/all_albums.js
@@ -1,6 +1,8 @@
 import gospels from "./new_testament_gospels";
 import apostles from "./new_testament_apostles";
 import epistles from "./new_testament_epistles";
+import prayerBook from "./prayer-book";
 
-const allAlbums = [gospels, apostles, epistles];
+const allAlbums = [gospels, apostles, epistles, prayerBook];
+
 export default allAlbums;
