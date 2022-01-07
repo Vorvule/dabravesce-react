fec2d724621cf556ea688b002306766671d4aa25
Vorvule <7105690@gmail.com>
2022-01-08 00:24:42 +0300

    Contacts added

--- src/albums/all_albums.js
+++ src/albums/all_albums.js
@@ -3,7 +3,8 @@ import apostles from "./new_testament_apostles";
 import epistles from "./new_testament_epistles";
 import prayerBook from "./prayer-book";
 import songs from "./songs";
+import contactData from "./contact-data";
 
-const allAlbums = [gospels, apostles, epistles, prayerBook, songs];
+const allAlbums = [gospels, apostles, epistles, prayerBook, songs, contactData];
 
 export default allAlbums;
