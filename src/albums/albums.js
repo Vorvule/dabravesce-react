978a078d103afb6775129821b2a566a86955eff1
Alexandre Vorvule <7105690@gmail.com>
2022-04-13 11:54:38 +0300

    apocalypse singled out of epistles

--- src/albums/albums.js
+++ src/albums/albums.js
@@ -1,11 +1,12 @@
 import { gospels } from "./gospels";
 import { apostles } from "./apostles";
 import { epistles } from "./epistles";
+import { apocalypse } from "./apocalypse"
 import { psalter } from "./psalter";
 import { prayers } from "./prayers";
 import { songs } from "./songs";
 import { contacts } from "./contacts";
 
-const allAlbums = [gospels, apostles, epistles, psalter, prayers, songs, contacts];
+const allAlbums = [gospels, apostles, epistles, apocalypse, psalter, prayers, songs, contacts];
 
 export { allAlbums };
