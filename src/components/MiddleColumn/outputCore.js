ad55b082422ffca2fb2d303effb6f4364f2bd2ab
Alexandre Vorvule <7105690@gmail.com>
2022-04-22 23:36:59 +0300

    setTitle() added

--- src/components/MiddleColumn/outputCore.js
+++ src/components/MiddleColumn/outputCore.js
@@ -1,14 +1,18 @@
 import { getChapterText } from "./getChapterText";
 
 const outputCore = (albumName, bookName, chapterName, chapterText) => {
+  const text = getChapterText(chapterText);
+
   document.getElementById("MiddleColumn_AlbumName").innerHTML = albumName;
   document.getElementById("MiddleColumn_BookName").innerHTML = bookName;
   document.getElementById("MiddleColumn_ChapterName").innerHTML = chapterName;
-  document.getElementById("MiddleColumn_ChapterParagraphs").innerHTML =
-    getChapterText(chapterText);
+  document.getElementById("MiddleColumn_ChapterParagraphs").innerHTML = text;
 
+  setTitle(bookName, chapterName);
   setDescription(albumName, bookName, chapterName);
 };
+
+// ======
 
 const setDescription = (albumName, bookName, chapterName) => {
   const description = albumName + ", " + bookName + ", " + chapterName;
@@ -16,5 +20,13 @@ const setDescription = (albumName, bookName, chapterName) => {
     .querySelector('meta[name="description"]')
     .setAttribute("content", description);
 };
+
+const setTitle = (bookName, chapterName) => {
+  const chapter = chapterName.split("<br />")[0];
+  const title = "Дабравесце — " + bookName + ", " + chapter;
+  document.title = title;
+};
+
+// ======
 
 export { outputCore };
