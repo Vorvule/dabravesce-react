5710009af147ada438fd81c748423f1ed3e4054e
Alexandre Vorvule <7105690@gmail.com>
2022-04-24 19:53:36 +0300

    Top buttons' paddings and size fixed

--- src/components/Top/TopHomeButton.jsx
+++ src/components/Top/TopHomeButton.jsx
@@ -4,12 +4,11 @@ import { showCore } from "../MiddleColumn/core/showCore";
 export const TopHomeButton = () => {
   return (
     <span
-      className="w3-bar-item w3-padding-large w3-large w3-hide-medium w3-hide-small w3-right"
+      className="w3-bar-item w3-hide-medium w3-hide-small w3-right"
       title="У пачатак"
       onClick={() => showCore(0, 0, 0)}
     >
-      <i className="fa fa-home"></i>
-      <strong className="w3-margin-left">Дамоў</strong>
+      <i className="fa fa-home w3-xxlarge"></i>
     </span>
   );
 };
