9bfca5661669f23efcabe9b75d94fd12783de072
Alexander Vorvul <7105690@gmail.com>
2022-04-29 22:46:35 +0300

    Book icon stays on small screens

--- src/components/Fixed/Top/left/TopMiddleButton.jsx
+++ src/components/Fixed/Top/left/TopMiddleButton.jsx
@@ -9,7 +9,7 @@ export const TopMiddleButton = () => {
       className="w3-bar-item w3-large w3-hide-large"
       onClick={() => handleClick("MiddleColumn")}
     >
-      <i className="fa fa-book fa-fw w3-hide-small w3-xlarge w3-margin-right"></i>
+      <i className="fa fa-book fa-fw w3-xlarge w3-margin-right"></i>
       <strong>Дабравесце</strong>
     </span>
   );
