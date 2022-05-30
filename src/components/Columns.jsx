0aa5692d6945680a0f12842a33cadccb40d1ccae
Alex <a7105690@yandex.by>
2022-05-30 22:59:30 +0300

    useEffect to set cols on keys change added

--- src/components/Columns.jsx
+++ src/components/Columns.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Cols } from "./Common/Cols";
 
 import { LeftColumn } from "./LeftColumn/LeftColumn";
 import { MiddleColumn } from "./MiddleColumn/MiddleColumn";
 import { RightColumn } from "./RightColumn/RightColumn";
 
 export const Columns = ({ keys, cols }) => {
+  useEffect(() => {
+    window.setCols(Cols.getInitial());
+  }, [keys]);
+
   return (
     <div className="w3-content page-container">
       <div className="w3-row">
